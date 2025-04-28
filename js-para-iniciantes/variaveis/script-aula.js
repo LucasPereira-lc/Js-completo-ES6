b5038eb172d52e3f931ee5f9a8ecf9849a3e031d
Lucas Pereira <lucasp304dolly@gmail.com>
2025-04-28 16:47:57 -0300

    Fiz o exercicio da primeira aula

---
+++ js-para-iniciantes/variaveis/script-aula.js
@@ -0,0 +1,48 @@
+var nome = 'Lucas'
+var idade = '18'
+var possuiFaculdade = 'false'
+console.log (nome, idade, possuiFaculdade)
+
+var preco = 5
+var totalComprado = 20
+var precoTotal = preco * totalComprado
+console.log(precoTotal)
+
+var nome = 'cadu',
+    idade = '19',
+    possuiFaculdade = 'false'
+console.log(nome, idade, possuiFaculdade)
+
+var precoAplicativo 
+console.log(precoAplicativo)
+
+// Inválido
+//var §nome;
+//var function;
+//var 1possuiFaculdade;
+
+// Válido
+var $selecionar;
+var _nome;
+var possuiFaculdadeNoExterior;
+
+//console.log(nome);
+// // retorna nome is not defined
+
+console.log(nome2);
+var nome2 = 'André';
+// Retorna undefined
+
+var profissao = 'Designer';
+console.log(profissao);
+// Retornar Designer
+
+var idade2 = 28;
+var idade2 = 29;
+
+let preco2 = 50;
+preco2 = 25;
+
+//const possuiFaculdade = true;
+//possuiFaculdade = false;
+// Retorna um erro
--- js-para-iniciantes/variaveis/script.js
+++ js-para-iniciantes/variaveis/script.js
@@ -1,48 +1,17 @@
+// Declarar uma variável com o seu nome
 var nome = 'Lucas'
-var idade = '18'
-var possuiFaculdade = 'false'
-console.log (nome, idade, possuiFaculdade)
+// Declarar uma variável com a sua idade
+var idade = 18
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'hamburguer'
+// Declarar 5 variáveis diferentes sem valores
+var time, 
+  cidade, 
+  pais, 
+  altura, 
+  profissao 
 
-var preco = 5
-var totalComprado = 20
-var precoTotal = preco * totalComprado
-console.log(precoTotal)
-
-var nome = 'cadu',
-    idade = '19',
-    possuiFaculdade = 'false'
-console.log(nome, idade, possuiFaculdade)
-
-var precoAplicativo 
-console.log(precoAplicativo)
-
-// Inválido
-//var §nome;
-//var function;
-//var 1possuiFaculdade;
-
-// Válido
-var $selecionar;
-var _nome;
-var possuiFaculdadeNoExterior;
-
-//console.log(nome);
-// // retorna nome is not defined
-
-console.log(nome2);
-var nome2 = 'André';
-// Retorna undefined
-
-var profissao = 'Designer';
-console.log(profissao);
-// Retornar Designer
-
-var idade2 = 28;
-var idade2 = 29;
-
-let preco2 = 50;
-preco2 = 25;
-
-//const possuiFaculdade = true;
-//possuiFaculdade = false;
-// Retorna um erro
+console.log (nome, idade, comidaFavorita)
