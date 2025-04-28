2894e3e79fb63ddd95df04723d76e5ef7edca6d3
Lucas Pereira <lucasp304dolly@gmail.com>
2025-04-28 17:21:15 -0300

    Tipos de Dados

---
+++ js-para-iniciantes/tipos-de-dados/script.js
@@ -0,0 +1,8 @@
+var nome = 'Lucas';
+console.log(typeof nome);
+// retorna string
+
+var nome = 'Lucas';
+var sobrenome = 'Pereira';
+var nomeCompleto = nome + ' ' + sobrenome;
+console.log(nomeCompleto)
