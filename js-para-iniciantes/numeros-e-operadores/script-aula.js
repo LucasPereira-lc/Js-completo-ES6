38ed53217d83c47d17801c48c016541af6f84fef
Lucas Pereira <lucasp304dolly@gmail.com>
2025-04-29 18:41:44 -0300

    Números e Operadores

---
+++ js-para-iniciantes/numeros-e-operadores/script-aula.js
@@ -0,0 +1,37 @@
+var idade = 28;
+var gols = 1000;
+var pi = 3.14; // ponto para decimal
+var exp = 2e10; // 20000000000
+
+var soma = 100 + 50; // 150
+var subtracao = 100 - 50; // 50
+var multiplicacao = 100 * 2; // 200
+var divisao = 100 / 2; // 50
+var expoente = 2 ** 4; // 16
+var modulo = 14 % 5; // 4
+
+var soma = '100' + 50; // 10050
+var subtracao = '100' - 50; // 50
+var multiplicacao = '100' * '2'; // 200
+var divisao = 'Comprei 10' / 2; // NaN (Not a Number)
+
+var numero = 80;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+var pesoPorDois = peso / 2 // NaN (Not a Number)
+
+var testenan = '100' / 2 
+console.log(isNaN(testenan))
+
+var total1 = 20 + 5 * 2; // 30
+var total2 = (20 + 5) * 2; // 50
+var total3 = 20 / 2 * 5; // 50
+var total4 = 10 + 10 * 2 + 20 / 2; // 40
+
+var x = 2
+x++
+console.log(x)
+
+var idade2 = '18'
+somarIdade = 3
+console.log(+idade2 + somarIdade)
---
+++ js-para-iniciantes/numeros-e-operadores/script.js
@@ -0,0 +1,25 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total)
+
+// Crie duas expressões que retornem NaN
+var n1 = 'tenho 2'
+var n2 = 'quero 5'
+console.log(n1 * 2)
+console.log(n2 - 4)
+
+// Somar a string '200' com o número 50 e retornar 250
+//  var string = '200'
+//  console.log(+string + 50)
+var soma = +'200' + 50
+console.log(soma)
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var n = 5
+console.log(++n)
+
+// Como dividir o peso por 2?
+var numero = +'80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+console.log(peso)
