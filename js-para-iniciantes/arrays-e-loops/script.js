3de56bae6ae44aa2866c1ca2d94eeea41cbf3398
Lucas Pereira <lucasp304dolly@gmail.com>
2025-05-02 19:18:47 -0300

    comecei o arrays e loops 1

---
+++ js-para-iniciantes/arrays-e-loops/script.js
@@ -0,0 +1,7 @@
+var videoGames = ['Switch', 'PS4', 'Xbox', 'aa']
+videoGames[0] // 'Switch'
+videoGames[2] // 'Xbox'
+
+videoGames.pop() // Remove o último item e retorna ele
+videoGames.push('Nintendo') // Adiciona ao final da array
+videoGames.length // 3
