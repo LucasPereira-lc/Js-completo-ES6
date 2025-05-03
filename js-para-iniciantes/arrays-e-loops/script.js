3e37d6888cef47f5634470e266959ecd2132cccb
Lucas Pereira <lucasp304dolly@gmail.com>
2025-05-03 16:28:47 -0300

    Aula: Arrays e Loops 1

--- js-para-iniciantes/arrays-e-loops/script.js
+++ js-para-iniciantes/arrays-e-loops/script.js
@@ -5,3 +5,13 @@ videoGames[2] // 'Xbox'
 videoGames.pop() // Remove o último item e retorna ele
 videoGames.push('Nintendo') // Adiciona ao final da array
 videoGames.length // 3
+
+for (var n = 10; n >= 1; n--) {
+  console.log(n)
+}
+
+var i = 1
+while (i <= 5) {
+  console.log(i)
+  i++
+}
