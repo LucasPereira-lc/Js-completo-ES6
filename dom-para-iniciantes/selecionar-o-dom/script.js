59f7af9c1bb3d44b51ee6f1e9ee20edf7ecd47ef
Lucas Pereira <lucasp304dolly@gmail.com>
2025-05-21 18:29:32 -0300

    Aula: Seleção de Elementos 1

---
+++ dom-para-iniciantes/selecionar-o-dom/script.js
@@ -0,0 +1,17 @@
+const animais = document.getElementById('animais')
+console.log(animais)
+
+const gridSection = document.getElementsByClassName('grid-section')
+console.log(gridSection)
+
+const primeiraLi = document.querySelector('li')
+console.log(primeiraLi) // retorna a primeira li do código
+
+const primeiraUl = document.querySelector('ul')
+console.log(primeiraUl) // retorna a primeira ul do código
+
+const linkInterno = document.querySelector('[href^="#"')
+console.log(linkInterno) // retorna o primeiro link interno do código
+
+const animaisImg = document.querySelectorAll('.animais img')
+console.log(animaisImg)
