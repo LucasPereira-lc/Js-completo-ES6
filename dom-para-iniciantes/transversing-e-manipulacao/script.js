f5b954f0f266bc832b0b1cfcc60abbbf03c5d6ab
Lucas Pereira <lucasp304dolly@gmail.com>
2025-06-10 11:02:45 -0300

    Comecei Traversing e Manipulação 2

--- dom-para-iniciantes/transversing-e-manipulacao/script.js
+++ dom-para-iniciantes/transversing-e-manipulacao/script.js
@@ -8,4 +8,14 @@ console.log(lista.children[--lista.children.length]) // último filho
 lista.querySelectorAll('li') // todas as li's
 console.log(lista.querySelector('li:last-child')) // último filho
 
-console.log(lista.childNodes)
+console.log(lista.childNodes) // lista de filhos node
+
+const animais = document.querySelector('.animais')
+const contato = document.querySelector('.contato')
+const titulo = contato.querySelector('.titulo')
+const mapa = document.querySelector('.mapa')
+
+// lista.appendChild(titulo)
+// contato.insertBefore(animais, mapa)
+// contato.removeChild(mapa) // remove o mapa do contato
+contato.replaceChild(animais, titulo)
