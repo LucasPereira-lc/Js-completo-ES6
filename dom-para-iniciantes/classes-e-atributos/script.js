9f153710480258f419d6687def5af0e8e2f0c933
Lucas Pereira <lucasp304dolly@gmail.com>
2025-07-22 17:48:07 -0300

    Revisão: Classes e Atributos

--- dom-para-iniciantes/classes-e-atributos/script.js
+++ dom-para-iniciantes/classes-e-atributos/script.js
@@ -1,25 +1,24 @@
 // Adicione a classe ativo a todos os itens do menu
-const itensMenu = document.querySelectorAll('.menu a')
-itensMenu.forEach((item) => {
+const menu = document.querySelectorAll('.menu ul li')
+
+menu.forEach((item) => {
   item.classList.add('ativo')
 })
 
 // Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
-itensMenu.forEach(function(item){
+menu.forEach((item) => {
   item.classList.remove('ativo')
+  menu[0].classList.add('ativo')
 })
-itensMenu[0].classList.add('ativo')
 
 // Verifique se as imagens possuem o atributo alt
-
 const imgs = document.querySelectorAll('img')
-imgs.forEach(function(item){
-  if (item.hasAttribute('alt')){
-    console.log(`${item} Possui alt`)
-  }else {
-    console.log(`${item} Não possui alt`)
-  }
+
+imgs.forEach((item) => {
+  console.log(item.hasAttribute('alt'))
 })
+
 // Modifique o href do link externo no menu
-const link = document.querySelector('.menu [href^="https://"]')
-link.setAttribute('href', 'https://www.youtube.com/')
+const linkExterno = document.querySelector('.menu a[href^="https"')
+
+linkExterno.setAttribute('href', 'https://www.youtube.com/')
