6fd0dad61206506d5df2ccd2fea50c03b03a0c43
Lucas Pereira <lucasp304dolly@gmail.com>
2025-07-23 17:09:32 -0300

    Revisão: Dimensões e Distâncias

--- dom-para-iniciantes/dimensoes-e-distancias/script.js
+++ dom-para-iniciantes/dimensoes-e-distancias/script.js
@@ -6,8 +6,8 @@ console.log(img.offsetTop)
 // Retorne a soma da largura de todas as imagens
 const imgs = document.querySelectorAll('img')
 let soma = 0
-imgs.forEach(function(img){
-  soma += img.offsetWidth
+imgs.forEach((img) => {
+  soma += img.clientWidth
 })
 console.log(soma)
 
@@ -15,18 +15,19 @@ console.log(soma)
 // o mínimo recomendado para telas utilizadas
 // com o dedo. (48px/48px de acordo com o google)
 const links = document.querySelectorAll('a')
-links.forEach(function(link){
-  if (link.offsetHeight >= 48 && link.offsetWidth >= 48) {
-  console.log(link, 'Tamanho recomendado')
+links.forEach((link) => {
+  if (link.offsetWidth >= 48 && link.offsetHeight >= 48) {
+    console.log(`${link} possui boa acessibilidade`)
   }else {
-  console.log(link, 'Tamanho não recomendado')
+    console.log(`${link} NÃO possui boa acessibilidade`)
   }
-}) 
+})
 
 // Se o browser for menor que 720px,
 // adicione a classe menu-mobile ao menu
 const menu = document.querySelector('.menu')
-const browser = window.matchMedia('(max-width: 720px)').matches
-if (browser) {
+const browser = window.matchMedia('(max-width: 720px')
+
+if (browser.matches) {
   menu.classList.add('menu-mobile')
 }
