16ca5c82a95843f5749ba0b5570139799f667975
Lucas Pereira <lucasp304dolly@gmail.com>
2025-07-29 16:07:40 -0300

    Revisão: Eventos

--- dom-para-iniciantes/eventos/script.js
+++ dom-para-iniciantes/eventos/script.js
@@ -4,41 +4,45 @@
 // o comportamento padrão desses links
 const linksInternos = document.querySelectorAll('a[href^="#"]')
 
-// function classeItem(e) {
-//   e.preventDefault()
-//   linksInternos.forEach((link) => {
-//     link.classList.remove('ativo')
-//   })
-//   e.currentTarget.classList.add('ativo')
-// }
-// linksInternos.forEach((link) => {
-//   link.addEventListener('click', classeItem)
-// })
+function addClasse(e) {
+  e.preventDefault()
+  linksInternos.forEach((link) => {
+    link.classList.remove('ativo')
+  })
+  e.currentTarget.classList.add('ativo')
+}
+
+linksInternos.forEach((link) => {
+  link.addEventListener('click', addClasse)
+})
 
 // Selecione todos os elementos do site começando a partir do body,
 // ao clique mostre exatamente quais elementos estão sendo clicados
-const elementos = document.querySelectorAll('body *')
+const todosElementos = document.querySelectorAll('body *')
 
-// function mostrar(e) {
-//   console.log(e.currentTarget)
-// }
-// elementos.forEach((element) => {
-//   element.addEventListener('click', mostrar)
+function mostrarElemento(e) {
+  console.log(e.currentTarget)
+}
+
+// todosElementos.forEach((e) => {
+//   e.addEventListener('click', mostrarElemento)
 // })
 
 // Utilizando o código anterior, ao invés de mostrar no console,
 // remova o elemento que está sendo clicado, o método remove() remove um elemento
-// function remove(e) {
-//   e.currentTarget.remove()
-// }
-// elementos.forEach((element) => {
-//   element.addEventListener('click', remove)
-// })
+function removerElemento(e) {
+  e.target.remove()
+}
+
+todosElementos.forEach((e) => {
+  e.addEventListener('click', removerElemento)
+})
 
 // Se o usuário clicar na tecla (t), aumente todo o texto do site. 
-function aumentar(e) {
+function aumentarTexto(e) {
   if (e.key === 't') {
-    document.documentElement.classList.toggle('texto-maior');
+    document.documentElement.classList.toggle('texto-maior')
   }
 }
-window.addEventListener('keydown', aumentar)
+
+window.addEventListener('keydown', aumentarTexto)
