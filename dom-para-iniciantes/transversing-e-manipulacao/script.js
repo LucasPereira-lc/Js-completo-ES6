36a190054c560ee66294c21383655c691a43a859
Lucas Pereira <lucasp304dolly@gmail.com>
2025-07-30 16:29:40 -0300

    Revisão: Traversing e Manipulação

--- dom-para-iniciantes/transversing-e-manipulacao/script.js
+++ dom-para-iniciantes/transversing-e-manipulacao/script.js
@@ -1,21 +1,19 @@
 // Duplique o menu e adicione ele em copy
 const menu = document.querySelector('.menu')
 const cloneMenu = menu.cloneNode(true)
-const copy = document.querySelector('.copy')
 
+const copy = document.querySelector('.copy')
 copy.appendChild(cloneMenu)
 
 // Selecione o primeiro DT da dl de Faq
-const faq = document.querySelector('.faq')
-const primeiroDt = faq.querySelector('dt')
-
-console.log(primeiroDt)
+const dl = document.querySelector('.faq-lista')
+const primeiroDt = dl.querySelector('dt')
 
 // Selecione o DD referente ao primeiro DT
-const próximoDD = primeiroDt.nextElementSibling
-
-console.log(próximoDD)
+const proximoDd = primeiroDt.nextElementSibling
 
 // Substitua o conteúdo html de .faq pelo de .animais
 const animais = document.querySelector('.animais')
+const faq = document.querySelector('.faq')
+
 faq.innerHTML = animais.innerHTML
