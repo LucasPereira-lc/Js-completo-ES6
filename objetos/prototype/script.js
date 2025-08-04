32b14ae4e5e421a2d1a4135db5a5a2a734974b32
Lucas Pereira <lucasp304dolly@gmail.com>
2025-08-04 18:58:57 -0300

    Aula: Prototype 1

--- objetos/prototype/script.js
+++ objetos/prototype/script.js
@@ -3,7 +3,17 @@ function Pessoa(nome, idade) {
   this.idade = idade
 }
 
+Pessoa.prototype.andar = function() {
+  return this.nome + ' andou'
+}
+
 const lucas = new Pessoa('Lucas', 18)
 
 console.log(Pessoa.prototype)
 console.log(lucas.prototype)
+
+// Acessam o mesmo método
+// mas __proto__ não terá
+// acesso ao this.nome
+// andre.andar();
+// andre.__proto__.andar();
