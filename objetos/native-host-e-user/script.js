216926b3c5400bfefcaf768d14c1d2f6fe9d09b0
Lucas Pereira <lucasp304dolly@gmail.com>
2025-08-27 11:47:44 -0300

    Ex: Native, Host e User

--- objetos/native-host-e-user/script.js
+++ objetos/native-host-e-user/script.js
@@ -1,7 +1,28 @@
-console.log(typeof Array.from !== 'undefined')
+// Liste 5 objetos nativos
+Object
+String
+Array
+Function
+Number
 
-if (typeof Array.from !== 'undefined') {
-  console.log('Existe')
-}else {
+// Liste 5 objetos do browser
+window
+history
+document
+HTMLAllCollection
+NodeList
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+
+if (typeof document.webkitHidden !== 'undefined') {
+  console.log("Existe")
+} else {
+  console.log('Não existe')
+}
+
+if (typeof document.webkitFullscreenElement !== 'undefined') {
+  console.log("Existe")
+} else {
   console.log('Não existe')
 }
