3c2196a96773a0ee95839ece85622f22293941ac
Lucas Pereira <lucasp304dolly@gmail.com>
2025-08-27 11:55:21 -0300

    Fazendo: String 1

---
+++ objetos/string/script.js
@@ -0,0 +1,11 @@
+const comida = 'Pizza'
+const agua = new String('Agua')
+
+const frase = 'A melhor comida';
+
+comida.length; // 5
+frase.length; // 15
+
+comida[0] // P
+frase[0] // A
+frase[frase.length - 1] // a
