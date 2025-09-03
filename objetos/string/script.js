ff8a5934017de825710991e76d1128175fc4aba5
Lucas Pereira <lucasp304dolly@gmail.com>
2025-09-03 18:07:25 -0300

    Aula: String 1

--- objetos/string/script.js
+++ objetos/string/script.js
@@ -16,3 +16,21 @@ const frase2 = 'A melhor linguagem é ';
 const linguagem = 'JavaScript';
 
 const fraseCompleta = frase2.concat(linguagem, '!!');
+
+
+const fruta = 'Banana';
+const listaFrutas = 'Melancia, Banana, Laranja';
+
+console.log(listaFrutas.includes(fruta, 10))
+
+console.log(fruta.endsWith('nana'))
+
+const transacao1 = 'Depósito de cliente';
+const transacao2 = 'Depósito de fornecedor';
+const transacao3 = 'Taxa de camisas';
+
+console.log(transacao1.slice(-2))
+
+const instrumento = 'Guitarra';
+
+console.log(fruta.lastIndexOf('a'))
